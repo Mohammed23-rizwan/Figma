d64d974a24704d88670c2645f041df1db04d6c0a
rizwan <rizwanedu03@gmail.com>
2025-09-16 18:40:18 +0530

    Day-6

---
+++ Design-6/script.js
@@ -0,0 +1,33 @@
+const slides = document.querySelectorAll(".slides");
+const dots = document.querySelectorAll(".dot");
+
+let index = 0;
+
+dots[1].style.opactiy = "1";
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", (e) => {
+    index = i;
+    removeopacity();
+    e.target.style.opacity = "1";
+    moveslide();
+  });
+});
+
+const removeopacity = () => {
+  dots.forEach((dot) => {
+    console.log('first')
+    dot.style.opacity = ".2";
+  });
+};
+
+const moveslide = () => {
+  slides.forEach((slide) => {
+    console.log("first");
+    slide.style.transform = `translateY(-${index * 100}%)`;
+  });
+};
